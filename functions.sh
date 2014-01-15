baa8e70ccbc64fbbddcdb827f5bfc33e58455c8e
George MacRorie <george.macrorie@semantico.com>
2014-01-15 19:55:55 +0000

    Some useful messages for guard-it useage

--- functions.sh
+++ functions.sh
@@ -1,11 +1,14 @@
-function guard-it () {
-  if [ $1 = "start" ]; then
+function guard-it {
+  if [[ $1 = "start" ]]; then
       screen -S guard -d -m bundle exec guard
-  elif [ $1 = "stop" ]; then
+      echo "Attempting to start guard in screen. Please check screen (screen -ls)"
+  elif [[ $1 = "stop" ]]; then
       screen -X -S guard kill
-  elif [ $1 = "check" ]; then
+      echo "Attempting to kill guard in screen. Please check screen (screen -ls)"
+      screen -ls
+  elif [[ $1 = "view" ]]; then
       screen -r guard
   else
-      clear
+      echo "Useage guard-it start | stop | view"
   fi
 }
